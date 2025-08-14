a9126197790bcbce97a357aa3b79abbb0cd699c2
Saria Khatun <sariajuiit@gmail.com>
2025-08-14 12:33:33 +0600

    helmet

--- src/pages/Home/Home/Banner/Banner.jsx
+++ src/pages/Home/Home/Banner/Banner.jsx
@@ -1,9 +1,9 @@
 import { Button } from "@/components/ui/button"
 import { Link } from "react-router-dom"
-import banner from '../../../../assets/banner (2).png'
+import banner from '../../../../assets/logoPet.png'
 const Banner = () => {
   return (
-    <section className="my-12 relative w-full lg:h-[85vh] flex items-center justify-center bg-gray-100 overflow-hidden rounded-2xl shadow-md">
+    <section className="mb-12 mt-8 relative w-full lg:h-[85vh] flex items-center justify-center bg-gray-100 overflow-hidden rounded-2xl shadow-md">
   <img
     src={banner} 
     alt="Pet Adoption Banner"
